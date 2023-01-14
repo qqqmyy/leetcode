430df54d477475f3af2151c08738f84e48a879ee
qqqmyy <993888344@qq.com>
2023-01-14 12:21:57 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/leetcode_zjy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_zjy.dir/main.cpp.o"
+  "leetcode_zjy"
+  "leetcode_zjy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode_zjy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
